aa5dbbb2039e57c77061ec90c17a9f65a15644d1
Matthias Friedrich <matt@mafr.de>
2008-06-26 10:46:58 +0200

    Backups user's home directory, no longer mine. Removed --delete switch. Added license.

--- backup.sh
+++ backup.sh
@@ -2,10 +2,25 @@
 #
 # A simple script for snapshot backups.
 #
+# Copyright (c) 2008, Matthias Friedrich <matt@mafr.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
 
-SOURCE_DIR=/home/matthias
+# Change these directories to whatever makes sense on your system!
+#
+SOURCE_DIR=$HOME
 DEST_DIR=/media/disk/backup
 
+# If the directory isn't there then the drive probably isn't mounted.
 if [ ! -e "$DEST_DIR" ]; then
 	echo "$0: directory $DEST_DIR doesn't exist" 2>&1
 	exit 1
@@ -18,6 +33,8 @@ test -e backup.2 && mv backup.2 backup.3
 test -e backup.1 && mv backup.1 backup.2
 test -e backup.0 && mv backup.0 backup.1
 
-rsync -a --delete --link-dest=../backup.1 "$SOURCE_DIR/"  backup.0/
+rsync --archive --link-dest=../backup.1 \
+	--filter="merge $HOME/.backup.rc" \
+	"$SOURCE_DIR/" backup.0/
 
 # EOF
