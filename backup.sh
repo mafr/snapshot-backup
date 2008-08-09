795e877180099abeb2e324b335c8472db2ccc2c8
Matthias Friedrich <matt@mafr.de>
2008-08-09 15:06:28 +0200

    Fixed a bug that changed permissions in backup directories (Mike). Backup to a temporary directory and move it afterwards (Mike). Added some more documentation.

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 # A simple script for snapshot backups.
 #
 # Copyright (c) 2008, Matthias Friedrich <matt@mafr.de>
+# Copyright (c) 2008, Michael Koehnlein <michael.koehnlein@googlemail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,18 +19,35 @@
 # Change these directories to whatever makes sense on your system!
 # KEEP_NUM is the number of backups (that is, snapshot directories) to keep.
 #
-KEEP_NUM=4
 SOURCE_DIR=$HOME
-DEST_DIR=/media/disk/backup
+DEST_DIR=/media/disk/backups
+KEEP_NUM=4
+
+TMP_DIRNAME=backup.tmp
 
 # Print an error message and exit.
 die() {
-	echo "$0: $1" 2>&1
+	echo "$(basename $0): $1" 2>&1
 	exit 1
 }
 
+
 # If the directory isn't there then the drive probably isn't mounted.
+#
 cd "$DEST_DIR" 2> /dev/null || die "cannot change to directory $DEST_DIR"
+
+
+# For safety reasons we backup in a temporary backup directory.
+# If a previous temporary backup directory exists, fail.
+#
+test -e "$TMP_DIRNAME" \
+	&& die "temporary backup directory exists: $DEST_DIR/$TMP_DIRNAME"
+
+# Create the backup.
+#
+rsync --archive --link-dest=../backup.0 \
+	--filter="merge $HOME/.backup.rc" \
+	"$SOURCE_DIR/" "$TMP_DIRNAME/" || die "cannot create backup $SOURCE_DIR"
 
 
 # Renumber all existing backups. backup.N is renamed to backup.N+1.
@@ -43,17 +61,19 @@ for i in $(seq $KEEP_NUM -1 1); do
 done
 
 
-rsync --archive --link-dest=../backup.1 \
-	--filter="merge $HOME/.backup.rc" \
-	"$SOURCE_DIR/" backup.0/ || die "cannot create backup $SOURCE_DIR"
+# Now that everything went well, rename the temporary backup directory to its
+# final destination.
+#
+mv "$TMP_DIRNAME" backup.0 || die "cannot rename $TMP_DIRNAME"
 
 
 # Delete the oldest backup. We have to adjust permissions first,
 # just in case there are directories we may not traverse or delete.
+# We don't adjust permissions of files because they are shared across backups!
 #
 OLDEST=backup.$KEEP_NUM
 if [ -e $OLDEST ]; then
-	chmod -R u+rwx $OLDEST && rm -rf $OLDEST \
+	find $OLDEST -type d | xargs chmod u+rwx && rm -rf $OLDEST \
 		|| die "cannot delete the oldest backup $OLDEST"
 fi
 
